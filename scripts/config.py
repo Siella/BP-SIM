a370a003e899103ad081eab2dd743090c8fcdafc
Любовь Елховская <lyubov.elkhovskaya@gmail.com>
2021-11-30 23:06:19 +0300

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,12 @@
 import configparser
+import os
 
+
+def get_pardir(path: str) -> str:
+    return path[:path.rfind('\\')]
+
+
+parent_cwd = get_pardir(os.path.dirname(__file__))
 config = configparser.ConfigParser()
-config.read("../config.ini")
+config.read(os.path.join(parent_cwd, 'config.ini'))
 path_to_file = config['Data']['path_to_file']
