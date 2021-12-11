6ad56286d37980ea0bf1372f11ccba29f73acef0
Любовь Елховская <lyubov.elkhovskaya@gmail.com>
2021-12-11 11:27:55 +0300

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,4 @@
-from scripts.bp_simulator import Simulator
+from scripts.bp_simulator import Simulator, Measurement
 from scripts.filter import Filter
 from scripts.patient import Patient
 from scripts.rules import Rule, arv_rule, base_rule, sd_rule
